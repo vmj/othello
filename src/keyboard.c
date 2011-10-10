1ec701735870048f2e97e5b3b667f674c81140d9
Mikko Värri <vmj@linuxbox.fi>
2011-10-11 01:29:15 +0300

    Fix warnings.

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <GL/glut.h>
 #include "common.h"
 #include "camera.h"
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <GL/glut.h>
 #include "display.h"
 #include "keyboard.h"
